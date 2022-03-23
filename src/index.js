baad1465e3e17932a885acfdf8d6375e91ec6a06
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-24 00:54:03 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { engine } = require('express-handlebars');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const app = express();
-const port = 5080;
+const port = process.env.port || 5080;
 
 const route = require('./routes');
 const db = require('./config/db');
@@ -23,12 +23,7 @@ app.use(methodOverride('_method'));
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs',engine({
-        extname: '.hbs',
-        helpers: {
-            sum: (a, b) => a + b,
-        }
-    }),
+app.engine('hbs',engine({ extname: '.hbs' }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
