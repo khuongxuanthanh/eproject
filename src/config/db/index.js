76a26dba92c07d589214a79bbdab67badd3b49e0
khuongxuanthanh <92875629+khuongxuanthanh@users.noreply.github.com>
2022-03-25 18:26:18 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://khuongxuanthanh-t2110e:t2110e*one@eproject-nodejs.kl0sv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/eProject-nodejs', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('Connect successfully!!!')    ;
+        console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
     }
